f47951b9a9a984ad386721adf739eb92fef8aaa5
taimurhafeez <taimurhafeez93@gmail.com>
2026-05-20 17:54:40 +0500

    Addressed Reviews: Seperate scenarios & using dynamic profile bundle

--- tests/e2e/parallel/main_test.go
+++ tests/e2e/parallel/main_test.go
@@ -855,92 +855,76 @@ func TestScanTailoredProfileIsDeprecated(t *testing.T) {
 	if err = f.WaitForScanStatus(f.OperatorNamespace, scanName, compv1alpha1.PhaseDone); err != nil {
 		t.Fatal(err)
 	}
+}
 
-	// SCENARIO 2: TailoredProfile extends a deprecated Profile - the TailoredProfile itself is not marked deprecated,
-	// but it should still trigger the deprecation warning because it extends a deprecated profile
-	// Create a ProfileBundle that will generate profiles
-	deprecatedPBName := "test-deprecated-pb"
-	deprecatedPB := &compv1alpha1.ProfileBundle{
+func TestScanTailoredProfileExtendsDeprecatedProfile(t *testing.T) {
+	t.Parallel()
+	f := framework.Global
+
+	pbName := framework.GetObjNameFromTest(t)
+	baselineImage := fmt.Sprintf("%s:%s", brokenContentImagePath, "deprecated_profile")
+	pb, err := f.CreateProfileBundle(pbName, baselineImage, framework.OcpContentFile)
+	if err != nil {
+		t.Fatalf("failed to create ProfileBundle: %s", err)
+	}
+	defer f.Client.Delete(context.TODO(), pb)
+
+	if err := f.WaitForProfileBundleStatus(pbName, compv1alpha1.DataStreamValid); err != nil {
+		t.Fatalf("failed waiting for the ProfileBundle to become available: %s", err)
+	}
+
+	profileList := &compv1alpha1.ProfileList{}
+	err = f.Client.List(context.TODO(), profileList,
+		client.InNamespace(f.OperatorNamespace),
+		client.MatchingLabels{compv1alpha1.ProfileBundleOwnerLabel: pbName})
+	if err != nil {
+		t.Fatalf("failed to list profiles for bundle %s: %s", pbName, err)
+	}
+
+	var deprecatedProfileName string
+	for _, p := range profileList.Items {
+		if p.Annotations[compv1alpha1.ProfileStatusAnnotation] == "deprecated" {
+			deprecatedProfileName = p.Name
+			break
+		}
+	}
+	if deprecatedProfileName == "" {
+		t.Fatal("no deprecated profile found in the bundle")
+	}
+
+	tpName := framework.GetObjNameFromTest(t) + "-tp"
+	tp := &compv1alpha1.TailoredProfile{
 		ObjectMeta: metav1.ObjectMeta{
-			Name:      deprecatedPBName,
+			Name:      tpName,
 			Namespace: f.OperatorNamespace,
-		},
-		Spec: compv1alpha1.ProfileBundleSpec{
-			ContentImage: contentImagePath,
-			ContentFile:  framework.OcpContentFile,
-		},
-	}
-	err = f.Client.Create(context.TODO(), deprecatedPB, nil)
-	if err != nil {
-		t.Fatal(err)
-	}
-	defer f.Client.Delete(context.TODO(), deprecatedPB)
-
-	// Wait for the ProfileBundle to be ready and create profiles
-	err = f.WaitForProfileBundleStatus(deprecatedPBName, compv1alpha1.DataStreamValid)
-	if err != nil {
-		t.Fatal(err)
-	}
-
-	// Find one of the profiles created by the ProfileBundle and annotate it as deprecated
-	// Using ocp4-cis as it should be available in the standard content
-	deprecatedProfileName := deprecatedPBName + "-cis"
-	deprecatedProfile := &compv1alpha1.Profile{}
-	err = f.Client.Get(context.TODO(), types.NamespacedName{Name: deprecatedProfileName, Namespace: f.OperatorNamespace}, deprecatedProfile)
-	if err != nil {
-		t.Fatal(err)
-	}
-
-	// Add deprecated annotation to the profile
-	deprecatedProfileCopy := deprecatedProfile.DeepCopy()
-	if deprecatedProfileCopy.Annotations == nil {
-		deprecatedProfileCopy.Annotations = make(map[string]string)
-	}
-	deprecatedProfileCopy.Annotations[compv1alpha1.ProfileStatusAnnotation] = "deprecated"
-	err = f.Client.Update(context.TODO(), deprecatedProfileCopy)
-	if err != nil {
-		t.Fatal(err)
-	}
-
-	// Create a TailoredProfile that extends the deprecated Profile
-	tpExtendsDeprecatedName := "test-tp-extends-deprecated"
-	tpExtendsDeprecated := &compv1alpha1.TailoredProfile{
-		ObjectMeta: metav1.ObjectMeta{
-			Name:      tpExtendsDeprecatedName,
-			Namespace: f.OperatorNamespace,
-			// NO deprecation annotation on the TailoredProfile itself
 		},
 		Spec: compv1alpha1.TailoredProfileSpec{
 			Extends:     deprecatedProfileName,
-			Title:       "TestScanTailoredProfileExtendsDeprecated",
-			Description: "TestScanTailoredProfileExtendsDeprecated",
-			// Not specifying EnableRules - just extending the profile as-is to test deprecation
+			Title:       "TestScanTailoredProfileExtendsDeprecatedProfile",
+			Description: "TestScanTailoredProfileExtendsDeprecatedProfile",
 		},
 	}
-	err = f.Client.Create(context.TODO(), tpExtendsDeprecated, nil)
+	err = f.Client.Create(context.TODO(), tp, nil)
 	if err != nil {
 		t.Fatal(err)
 	}
-	defer f.Client.Delete(context.TODO(), tpExtendsDeprecated)
+	defer f.Client.Delete(context.TODO(), tp)
 
-	// Wait for TailoredProfile to be ready before creating SSB
-	err = f.WaitForTailoredProfileStatus(f.OperatorNamespace, tpExtendsDeprecatedName, compv1alpha1.TailoredProfileStateReady)
-	if err != nil {
+	if err = f.WaitForTailoredProfileStatus(f.OperatorNamespace, tpName, compv1alpha1.TailoredProfileStateReady); err != nil {
 		t.Fatal(err)
 	}
 
-	// Create SSB for second scenario
-	suiteName2 := framework.GetObjNameFromTest(t) + "-extends-deprecated"
-	ssb2 := &compv1alpha1.ScanSettingBinding{
+	suiteName := framework.GetObjNameFromTest(t)
+	ssb := &compv1alpha1.ScanSettingBinding{
 		ObjectMeta: metav1.ObjectMeta{
-			Name:      suiteName2,
+			Name:      suiteName,
 			Namespace: f.OperatorNamespace,
 		},
 		Profiles: []compv1alpha1.NamedObjectReference{
 			{
 				APIGroup: "compliance.openshift.io/v1alpha1",
 				Kind:     "TailoredProfile",
-				Name:     tpExtendsDeprecatedName,
+				Name:     tpName,
 			},
 		},
 		SettingsRef: &compv1alpha1.NamedObjectReference{
@@ -949,20 +933,18 @@ func TestScanTailoredProfileIsDeprecated(t *testing.T) {
 			Name:     "default",
 		},
 	}
-	err = f.Client.Create(context.TODO(), ssb2, nil)
+	err = f.Client.Create(context.TODO(), ssb, nil)
 	if err != nil {
 		t.Fatal(err)
 	}
-	defer f.Client.Delete(context.TODO(), ssb2)
+	defer f.Client.Delete(context.TODO(), ssb)
 
-	// Wait for deprecation warning for the Profile being extended
-	scanName2 := tpExtendsDeprecatedName
-	if err = f.WaitForProfileDeprecatedWarning(t, scanName2, deprecatedProfileName); err != nil {
+	scanName := tpName
+	if err = f.WaitForProfileDeprecatedWarning(t, scanName, deprecatedProfileName); err != nil {
 		t.Fatal(err)
 	}
 
-	// Verify scan completes successfully
-	if err = f.WaitForScanStatus(f.OperatorNamespace, scanName2, compv1alpha1.PhaseDone); err != nil {
+	if err = f.WaitForScanStatus(f.OperatorNamespace, scanName, compv1alpha1.PhaseDone); err != nil {
 		t.Fatal(err)
 	}
 }
